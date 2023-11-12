069a533285ec1804b14c22a3d91e27ea1c6fa8e9
afying <150595352+afying@users.noreply.github.com>
2023-11-12 17:28:05 +0500

    Add files via upload

---
+++ Npc.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : MonoBehaviour
+{
+    //здоровье Npc
+    public int health = 5;
+
+    //уровень Npc
+    public int level = 1;
+
+    //скорость Npc
+    public float speed = 1.2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
